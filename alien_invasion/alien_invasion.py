e08b188166ea29a25befa38f0c95beb1302fe0ea
sonu surendran <sonubusiness93@gmail.com>
2025-07-20 13:56:14 +0530

    Display Window - Done, Setting file - Created

---
+++ alien_invasion/alien_invasion.py
@@ -0,0 +1,38 @@
+import sys
+import pygame
+
+from settings import Settings
+
+class AlienInvasion:
+    """Overall Class to manage game assets and behavior."""
+
+    def __init__(self):
+        """Initialize the game, and create game resources"""
+        pygame.init()
+        self.settings = Settings()
+        self.clock = pygame.time.Clock()
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("Alien Invasion")
+
+        #Set the background colour
+        self.bg_color = (self.settings.bg_color)
+
+    def run_game(self):
+        """Start the main loop the game"""
+        while True:
+            #Watch for keyboard and mouse events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+                
+            self.screen.fill(self.bg_color)
+            
+            #Make the most recently drawn screen visible
+            pygame.display.flip()
+            self.clock.tick(60)
+
+if __name__ == '__main__':
+    #Make a game instance and run the game
+    ai = AlienInvasion()
+    ai.run_game()
